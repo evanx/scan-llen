d75a9bf0b30f7387bc79a4ffe0dc6701ed91ca5d
Evan Summers <evan.summers@gmail.com>
2017-02-25 00:58:06 +0000

    initial

--- bin/test.sh
+++ bin/test.sh
@@ -1,48 +1,58 @@
 
+set -u -e
+
+PATH=$PATH:./node_modules/.bin
+
 name='scan-llen'
 network="$name-network"
 redisName="$name-redis"
 
-removeContainers() {
-    for name in $@
-    do
-      if docker ps -a -q -f "name=/$name" | grep '\w'
-      then
-        docker rm -f `docker ps -a -q -f "name=/$name"`
-      fi
-    done
+run
+
+run() {
+    tearDown
+    setUp
+    sleep 1
+    populate
+    build
+    run
+    sleep 1
+    test
+    tearDown
 }
 
-removeNetwork() {
-    if docker network ls -q -f name=^$network | grep '\w'
-    then
-      docker network rm $network
-    fi
+tearDown() {
+  remove-containers $redisName
+  remove-network $network
 }
 
-(
-  removeContainers $redisName
-  removeNetwork
-  set -u -e -x
-  sleep 1
+setUp() {
   docker network create -d bridge scan-llen-network
   redisContainer=`docker run --network=scan-llen-network \
       --name $redisName -d redis`
   redisHost=`docker inspect $redisContainer |
       grep '"IPAddress":' | tail -1 | sed 's/.*"\([0-9\.]*\)",/\1/'`
-  sleep 1
+}
+
+populate() {
   redis-cli -h $redisHost lpush list1 1
   redis-cli -h $redisHost lpush list2 1
   redis-cli -h $redisHost lpush list2 2
   redis-cli -h $redisHost keys '*'
+}
+
+build() {
   docker build -t scan-llen https://github.com/evanx/scan-llen.git
+}
+
+run() {
   docker run --name scan-llen-instance --rm -i \
     --network=scan-llen-network \
     -e host=$redisHost \
     -e pattern='list*' \
-    scan-lleb
-  sleep 2
+    scan-llen
+}
+
+test() {
   redis-cli -h $redisHost keys '*'
-  docker rm -f $redisName
-  docker network rm $network
-)
+}
